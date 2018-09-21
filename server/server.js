0a6cc1ce924f4a8e39dc6c4509ad7f5918df0be2
Ishan Bhargava <ishantheperson@users.noreply.github.com>
2018-09-21 19:51:05 -0400

    changed server to typescript

--- server/server.js
+++ server/server.js
@@ -1,7 +1,5 @@
-const express = require('express');
-const app = express();
-const PORT = 2000;
-
+var express = require('express');
+var app = express();
+var PORT = 2000;
 app.use(express.static('./client/'));
-
 app.listen(PORT);
---
+++ server/server.ts
@@ -0,0 +1,7 @@
+const express = require('express');
+const app = express();
+const PORT = 2000;
+
+app.use(express.static('./client/'));
+
+app.listen(PORT);
