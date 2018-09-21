ac23602a45e9538369090e9d136b358840559027
Ishan Bhargava <ishantheperson@users.noreply.github.com>
2018-09-21 19:57:19 -0400

    typescript on the server seems annoying

--- server/server.js
+++ server/server.js
@@ -1,5 +1,7 @@
-var express = require('express');
-var app = express();
-var PORT = 2000;
+const express = require('express');
+const app = express();
+const PORT = 2000;
+
 app.use(express.static('./client/'));
+
 app.listen(PORT);
--- server/server.ts
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const app = express();
-const PORT = 2000;
-
-app.use(express.static('./client/'));
-
-app.listen(PORT);
