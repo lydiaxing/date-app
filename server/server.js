0309f0c82c607b99e747d7484df8f28269b468f6
Ishan Bhargava <ishantheperson@users.noreply.github.com>
2018-09-21 20:56:10 -0400

    more socket

--- server/server.js
+++ server/server.js
@@ -2,19 +2,25 @@ const express = require('express');
 
 const PORT = process.env.PORT || 2000;
 
-const app = express();
-const http = require('http').Server(app);
-const io = require('socket.io')(http);
+let app = express();
+let http = require('http').Server(app);
+let io = require('socket.io')(http);
 
 app.use(express.static('./client/'));
 
+let users = [];
+
 io.on('connection', socket => {
    console.log(socket);
+   
+   socket.on('getUsers', socket => {
+     socket.emit('userList', users);
+   });
 });
 
 io.on('getUsers', socket => {
   console.log('get users');
-})
+});
 
 http.listen(PORT, () => {
    console.log("Listening on", PORT);
