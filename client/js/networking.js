869eccc6898b9f81e64fcb8d08deaa14bcd7b9e6
Ishan Bhargava <ishantheperson@users.noreply.github.com>
2018-09-21 21:28:55 -0400

    Added documentation

--- client/js/networking.js
+++ client/js/networking.js
@@ -1,4 +1,8 @@
 class Networking {
+  /**
+   * Container object for all client side networking operatinos
+   * @param name The users display name
+   */
   constructor(name) {
     this.name = name;
     this.socket = io({ query: { name: name }});
@@ -7,10 +11,17 @@ class Networking {
     this.socket.on('userList', this.receiveUsers);
   }
   
+  /**
+   * Refreshes the list of users
+   */
   queryUsers() {
     this.socket.emit('getUsers');
   }
   
+  /**
+   * Receives user data
+   * @param data List of users received from the server
+   */
   receiveUsers(data) {
     this.otherUsers = data.filter(name => name !== this.name);
   }
