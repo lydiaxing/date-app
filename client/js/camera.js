d2c0456e0646f6bd3b81ea8a01eb30802833a359
cappuccinocat <xinglydia@gmail.com>
2018-09-21 20:09:59 -0400

    added empty ajax

--- client/js/camera.js
+++ client/js/camera.js
@@ -1,8 +1,20 @@
 const player = document.getElementById('player');
+const canvas = document.getElementById('canvas');
+const context = canvas.getContext('2d');
 
 const constraints = {
   video: true,
 };
+
+setInterval(function() {
+  context.drawImage(player, 0, 0, canvas.width, canvas.height)
+  var data = canvas.toDataURL();
+  $.ajax({
+    url: "",
+    method: "POST",
+    data: data
+  });
+}, 3000);
 
 navigator.mediaDevices.getUserMedia(constraints)
   .then((stream) => {
