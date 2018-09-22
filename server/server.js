0626fc1d924a2f2a64f7cf3afe2e40957ee1b32f
Ishan Bhargava <ishantheperson@users.noreply.github.com>
2018-09-21 23:19:22 -0400

    Started working on sessions

--- server/server.js
+++ server/server.js
@@ -12,23 +12,50 @@ class User {
   constructor(name, socket) {
     this.name = name;
     this.socket = socket;
+    this.isInSession = false;
   }
 }
 
 class Session {
-
+  constructor(user1, user2) {
+    this.user1 = user1;
+    this.user2 = user2;
+    
+    emitStart(this.user1); emitStart(this.user2);
+  }
+  
+  emitStart(user) {
+    user.socket.emit('start', {
+      // todo: data
+    })
+  }
 }
 
-let users = []
+let users = [];
+let sessions = [];
 
 io.on('connection', socket => {
-   users.push(new User(socket.handshake.query.name, socket));
-   
-   socket.on('getUsers', () => {
-     socket.emit('userList', getUsersList());
-   });
+  let user = new User(socket.handshake.query.name, socket);
+  users.push(user);
+  
+  socket.on('getUsers', () => {
+    socket.emit('userList', getPossibleUsersList());
+  });
+  
+  socket.on('initiateSession', (otherName) => {
+    // in the future we need to make sure the user exists
+    // otherwise malicious users can exploit this.
+    let other = findUser(otherName);
+    let session = new Session(user, other);
+    sessions.push(session);
+  });
 });
 
+/**
+ * Finds the User object in the array
+ * @param name The name of the user
+ * @returns {User} Found user (undefined behaviour if name is not in the array)
+ */
 function findUser(name) {
   return users.find(user => user.name === name);
 }
@@ -37,16 +64,20 @@ function findUser(name) {
  * Returns the users list without the sockets.
  * This helps avoid stack errors
  */
-function getUsersList() {
+function getPossibleUsersList() {
   let result = [];
+  
   for (let user of users) {
-    result.push({
-      name: user.name
-    });
+    if (!user.isInSession) {
+      result.push({
+        name: user.name
+      });
+    }
   }
+  
   return result;
 }
 
 http.listen(PORT, () => {
-   console.log("Listening on", PORT);
+  console.log("Listening on", PORT);
 });
