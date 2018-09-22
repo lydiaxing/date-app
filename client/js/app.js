9cb231f58d152e31ab9b804bed1a406daf749ea5
Ishan Bhargava <ishantheperson@users.noreply.github.com>
2018-09-21 21:56:34 -0400

    added knockout js

--- client/js/app.js
+++ client/js/app.js
@@ -1,7 +1,19 @@
+class DatingApp {
+  constructor() {
+    this.currentScreen = ko.observable('welcome');
+  }
+}
+
+// TODO: move into $() for safety
+const app = new DatingApp();
+
 $(() => {
-  const networking;
-  // Click button
-  document.getElementById('connect').addEventListener('click', () => {
+  ko.applyBindings(app);
   
+  let networking;
+  // Connect button
+  document.getElementById('connect').addEventListener('click', () => {
+    const name = document.getElementById('name').value;
+    networking = new Networking(name);
   });
 });
